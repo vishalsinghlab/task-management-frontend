131fd7e7b3889b165de44098542518a896f32087
vishal-s-gortnm <vishal.s@gortnm.in>
2026-05-10 14:12:49 +0530

    feat: setup Angular frontend structure and authentication pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'register',
+    component: Register,
+  },
+];
